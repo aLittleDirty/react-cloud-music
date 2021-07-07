073d433307dd9b981ec8d1f4c7d8e6a0d4e9c5cc
zhansiting <zhansiting@hdzn.cn>
2021-07-07 11:57:45 +0800

    新增基础路由及多个初始页面

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import { GlobalStyle } from './style'
-import { IconStyle } from './assets/iconfont/iconfont' 
+import { IconStyle } from './assets/iconfont/iconfont'
+import { renderRoutes } from 'react-router-config'
+import routes from './routes/index.js';
+import { HashRouter } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
+    <HashRouter>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
-      <i className='iconfont'>&#xe62b;</i>
-    </div>
+      { renderRoutes(routes) }
+    </HashRouter>
   );
 }
 
